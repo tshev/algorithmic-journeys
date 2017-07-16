6821cd8e234d1fa3696543ba01df922a33288223
Taras Shevchenko <cgeek95@gmail.com>
2017-07-16 21:44:28 +0300

    Update

--- presentation/metropolis/code/intersect.py
+++ presentation/metropolis/code/intersect.py
@@ -1,10 +1,8 @@
 def intersect(x, y):
-    if len(x) > len(y):
-        x, y = y, x
+    if len(x) > len(y): x, y = y, x
     for i, v in enumerate(x):
-        if v in y:
-            print(i)
-            yield v
+        if v in y: yield v; print(i);
 
-print(set(intersect(set([1, 2]), set(range(10000)))))
+x, y = set([1, 2]), set(range(10**7))
+print(set(intersect(x, y)))
 
